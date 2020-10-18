c12f40bac78abba6dc5b94fc17f7b852502f7a09
Michael Clark <michaeljclark@mac.com>
2020-10-18 17:36:27 +1300

    zhashmap: change data_type to a struct

--- src/hashmap.h
+++ src/hashmap.h
@@ -48,17 +48,22 @@ struct hashmap
     static inline _Hash _hasher;
     static inline _Pred _compare;
 
+    struct data_type {
+        Key first;
+        Value second;
+    };
+
     typedef Key key_type;
     typedef Value mapped_type;
     typedef std::pair<Key, Value> value_type;
     typedef _Hash hasher;
     typedef _Pred key_equal;
-    typedef value_type& reference;
-    typedef const value_type& const_reference;
+    typedef data_type& reference;
+    typedef const data_type& const_reference;
 
     size_t count;
     size_t limit;
-    value_type *data;
+    data_type *data;
     uint64_t *bitmap;
 
     /*
@@ -77,8 +82,8 @@ struct hashmap
         }
         iterator& operator++() { i = step(i+1); return *this; }
         iterator& operator++(int) { i = step(i) + 1; return *this; }
-        value_type& operator*() { i = step(i); return h->data[i]; }
-        value_type* operator->() { i = step(i); return &h->data[i]; }
+        data_type& operator*() { i = step(i); return h->data[i]; }
+        data_type* operator->() { i = step(i); return &h->data[i]; }
         bool operator==(const iterator &o) const { return h == o.h && i == o.i; }
         bool operator!=(const iterator &o) const { return h != o.h || i != o.i; }
     };
@@ -90,13 +95,13 @@ struct hashmap
     inline hashmap() : hashmap(default_size) {}
     inline hashmap(size_t initial_size) : count(0), limit(initial_size)
     {
-        size_t data_size = sizeof(value_type) * initial_size;
+        size_t data_size = sizeof(data_type) * initial_size;
         size_t bitmap_size = initial_size >> 2;
         size_t total_size = data_size + bitmap_size;
 
         assert(is_pow2(initial_size));
 
-        data = (value_type*)malloc(total_size);
+        data = (data_type*)malloc(total_size);
         bitmap = (uint64_t*)((char*)data + data_size);
         memset(data, 0, total_size);
     }
@@ -143,24 +148,24 @@ struct hashmap
      * the implementation
      */
 
-    void resize_internal(value_type *old_data, uint64_t *old_bitmap,
+    void resize_internal(data_type *old_data, uint64_t *old_bitmap,
                          size_t old_size, size_t new_size)
     {
-        size_t data_size = sizeof(value_type) * new_size;
+        size_t data_size = sizeof(data_type) * new_size;
         size_t bitmap_size = new_size >> 2;
         size_t total_size = data_size + bitmap_size;
 
         assert(is_pow2(new_size));
 
-        data = (value_type*)malloc(total_size);
+        data = (data_type*)malloc(total_size);
         bitmap = (uint64_t*)((char*)data + data_size);
         limit = new_size;
         memset(data, 0, total_size);
 
         size_t i = 0;
-        for (value_type *v = old_data; v != old_data + old_size; v++, i++) {
+        for (data_type *v = old_data; v != old_data + old_size; v++, i++) {
             if ((bitmap_get(old_bitmap, i) & occupied) != occupied) continue;
-            for (size_t j = key_index(v->first);; j = (j+1) & index_mask()) {
+            for (size_t j = key_index(v->first); ; j = (j+1) & index_mask()) {
                 if ((bitmap_get(bitmap, j) & occupied) != occupied) {
                     bitmap_set(bitmap, j, occupied);
                     data[j] = *v;
@@ -174,7 +179,7 @@ struct hashmap
 
     void clear()
     {
-        size_t data_size = sizeof(value_type) * limit;
+        size_t data_size = sizeof(data_type) * limit;
         size_t bitmap_size = limit >> 2;
         size_t total_size = data_size + bitmap_size;
         memset(data, 0, total_size);
@@ -189,11 +194,11 @@ struct hashmap
         for (size_t i = key_index(v.first); ; i = (i+1) & index_mask()) {
             if ((bitmap_get(bitmap, i) & occupied) != occupied) {
                 bitmap_set(bitmap, i, occupied);
-                data[i] = v;
+                data[i] = data_type{v.first, v.second};
                 count++;
                 if (load() > load_factor) {
                     resize_internal(data, bitmap, limit, limit << 1);
-                    for (i = key_index(v.first);; i = (i+1) & index_mask()) {
+                    for (i = key_index(v.first); ; i = (i+1) & index_mask()) {
                         bitmap_state state = bitmap_get(bitmap, i);
                              if (state == available) abort();
                         else if (state == deleted); /* skip */
